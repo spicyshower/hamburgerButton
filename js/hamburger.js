9c902aa743e38fcd984743a6b159f3151d0cb80f
Harrie Chon <j4314449901@gmail.com>
2015-06-22 08:26:15 +0900

    add shape

--- js/hamburger.js
+++ js/hamburger.js
@@ -25,3 +25,30 @@ $(function(){
     });
 });
 
+// shape first
+$(function(){
+    $(".Fshape").click(function(){
+        $(".shape1").toggleClass("shapeclick1"),
+        $(".shape2").toggleClass("shapeclick2"),
+        $(".shape3").toggleClass("shapeclick3")
+    });
+});
+
+// shape second
+$(function(){
+    $(".Sshape").click(function(){
+        $(".shape4").toggleClass("shapeclick4"),
+        $(".shape5").toggleClass("shapeclick5"),
+        $(".shape6").toggleClass("shapeclick6")
+    });
+});
+
+// shape third
+$(function(){
+    $(".Tshape").click(function(){
+        $(".shape7").toggleClass("shapeclick7"),
+        $(".shape8").toggleClass("shapeclick8"),
+        $(".shape9").toggleClass("shapeclick9")
+    });
+});
+
