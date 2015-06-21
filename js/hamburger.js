3aa336cf1dfc7a480e088ca637b2e4518268df1c
Harrie Chon <j4314449901@gmail.com>
2015-06-21 11:25:26 +0900

    hamburger button css

---
+++ js/hamburger.js
@@ -0,0 +1,27 @@
+// transition first
+$(function(){
+    $(".Fmenu").click(function(){
+        $(".menu1").toggleClass("menuclick1"),
+        $(".menu2").toggleClass("menuclick2"),
+        $(".menu3").toggleClass("menuclick3")
+    });
+});
+
+// transition second
+$(function(){
+    $(".Smenu").click(function(){
+        $(".menu4").toggleClass("menuclick4"),
+        $(".menu5").toggleClass("menuclick5"),
+        $(".menu6").toggleClass("menuclick6")
+    });
+});
+
+// transition third
+$(function(){
+    $(".Tmenu").click(function(){
+        $(".menu7").toggleClass("menuclick7"),
+        $(".menu8").toggleClass("menuclick8"),
+        $(".menu9").toggleClass("menuclick9")
+    });
+});
+
